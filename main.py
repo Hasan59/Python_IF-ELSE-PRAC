ff9b1e85f55d060cfcef526c9920634f55b7a8ad
Hasan Mahmud <1059hasan@gmail.com>
2022-06-15 16:36:17 +0600

    Matir Kanna

--- main.py
+++ main.py
@@ -18,3 +18,26 @@ if b > a:
       print("a and b are equal")
     else:
       print("a is greater than b")
+
+      a = 200
+      b = 33
+      if b > a:
+        print("b is greater than a")
+      else:
+        print("b is not greater than a")
+
+        if a > b: print("a is greater than b")
+
+        a = 2
+        b = 330
+        print("A") if a > b else print("B")
+
+        a = 330
+        b = 330
+        print("A") if a > b else print("=") if a == b else print("B")
+
+        a = 200
+        b = 33
+        c = 500
+        if a > b and c > a:
+          print("Both conditions are True")
