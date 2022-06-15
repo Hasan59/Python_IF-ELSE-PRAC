b38916bd4b7311bbdbf948b89bff29193fbe5cba
Hasan Mahmud <1059hasan@gmail.com>
2022-06-15 16:47:32 +0600

    Matir Pinjira

--- main.py
+++ main.py
@@ -41,3 +41,21 @@ if b > a:
         c = 500
         if a > b and c > a:
           print("Both conditions are True")
+
+          a = 200
+          b = 33
+          c = 50
+          x = 41
+
+          if x > 10:
+            print("Above ten,")
+            if x > 20:
+              print("and also above 20!")
+            else:
+              print("but not above 20.")
+              a = 33
+              b = 200
+
+              if b > a:
+                pass
+
